bd952cc8a48b49c8ad080b248f1e92c0eb39d898
CehhGhost <ghost5016628@mail.ru>
2025-03-30 14:19:12 +0300

    check if github actions are still working

--- backend/src/main/java/com/backend/crosswords/CrosswordsApplication.java
+++ backend/src/main/java/com/backend/crosswords/CrosswordsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-
+// TODO понять, что происходит с гитхабом
 @SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
 public class CrosswordsApplication {
 
