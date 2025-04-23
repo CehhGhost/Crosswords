33f98987469a7a59ff3df7536e10c1d6d4cc00ce
CehhGhost <ghost5016628@mail.ru>
2025-04-23 12:18:53 +0300

    added pair

---
+++ backend/src/main/java/com/backend/crosswords/config/Pair.java
@@ -0,0 +1,30 @@
+package com.backend.crosswords.config;
+
+public class Pair<T, F> {
+    private T first;
+    private F second;
+
+    public Pair() {
+    }
+
+    public Pair(T first, F second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public F getSecond() {
+        return second;
+    }
+
+    public void setSecond(F second) {
+        this.second = second;
+    }
+}
