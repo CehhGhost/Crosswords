24befcbf0806dd9817a639bb7a73d59a06b7d890
CehhGhost <ghost5016628@mail.ru>
2025-05-02 19:38:18 +0300

    added one source type: ENG

--- backend/src/main/java/com/backend/crosswords/corpus/enums/Language.java
+++ backend/src/main/java/com/backend/crosswords/corpus/enums/Language.java
@@ -1,7 +1,8 @@
 package com.backend.crosswords.corpus.enums;
 
 public enum Language {
-    RU;
+    RU,
+    ENG;
     public static Language getValueOf(String language) {
         if (language == null || language.isEmpty()) {
             throw new IllegalArgumentException("Language's name can't be null or empty!");
