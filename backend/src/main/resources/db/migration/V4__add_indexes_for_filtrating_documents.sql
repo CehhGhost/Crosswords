8556c578bc00ea11dc2507828031980a5cb1baa2
CehhGhost <ghost5016628@mail.ru>
2025-06-06 03:56:16 +0300

    small fix

--- backend/src/main/resources/db/migration/V4__add_indexes_for_filtrating_documents.sql
+++ backend/src/main/resources/db/migration/V4__add_indexes_for_filtrating_documents.sql
@@ -1,6 +1,6 @@
-CREATE INDEX idx_documents_date ON _documents(date);
-CREATE INDEX idx_documents_date ON _documents(source);
-CREATE INDEX idx_documents_source ON _documents(date, source);
+CREATE INDEX IF NOT EXISTS idx_documents_date ON _documents(date);
+CREATE INDEX IF NOT EXISTS idx_documents_source ON _documents(source);
+CREATE INDEX IF NOT EXISTS idx_documents_date_source ON _documents(date, source);
 
-CREATE INDEX idx_tags_name ON _tags(name);
-CREATE INDEX idx_docs_tags_doc_id ON _docs_tags(doc_id);
+CREATE INDEX IF NOT EXISTS idx_tags_name ON _tags(name);
+CREATE INDEX IF NOT EXISTS idx_docs_tags_doc_id ON _docs_tags(doc_id);
